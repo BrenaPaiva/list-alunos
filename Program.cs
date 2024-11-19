e6ab872642262dce2710a1ff1b4d4b9b56f45168
Brena Paiva <bpaiva@uniaoquimica.com.br>
2024-11-19 13:47:26 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using c_.Models;
+
+Pessoa pessoa1 = new Pessoa("Gustavo", "Siqueira");
+Pessoa pessoa2 = new Pessoa("Brena", "Paiva");
+
+
+// pessoa1.Profissao = "Analista de Sistemas";
+// pessoa1.Idade = 24;
+// pessoa1.Apresentar();
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(pessoa1);
+cursoDeIngles.AdicionarAluno(pessoa2);
+cursoDeIngles.ListarAlunos();
